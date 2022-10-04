f69f43648f489ca25e6d546ffac85c0c6b6eb48c
a4amber <aachow@princeton.edu>
2022-10-04 00:07:51 -0400

    remove excess comments

--- stra.c
+++ stra.c
@@ -3,10 +3,7 @@
 #include "str.h"
 #include <assert.h>
 
-
-/*The Str_getLength() function calculates the length 
-of the string in pcSrc[], excluding the terminating null byte. 
-Return length as size_t*/
+/*----------------------------------------------*/
 size_t Str_getLength(const char pcSrc[]){
    size_t uLength = 0;
    assert(pcSrc != NULL);
@@ -15,10 +12,7 @@ size_t Str_getLength(const char pcSrc[]){
    return uLength;
 }
 
-/*The Str_copy() function copies the string in pcSrc[],
-including the terminating null byte, to the buffer dest[].
-The strings may not overlap and the destination string dest[]
-must be large enough to recieve the copy. Return dest[0]*/
+/*----------------------------------------------*/
 char* Str_copy(char dest[], const char pcSrc[]){
    assert(pcSrc != NULL);
    for (size_t i = 0; pcSrc[i] != '\0', i++){
@@ -27,11 +21,7 @@ char* Str_copy(char dest[], const char pcSrc[]){
    return dest[0];
 } 
 
-/*The Str_concat() function appends the pcSrc[] string
-to the dest string, overwriting the terminating null byte. 
-The strings may not overlap, and the dest string must have
-enough space for teh result. If dest is not large enough, 
-program behavior is unpredictable. Return dest[0]*/
+/*----------------------------------------------*/
 char* Str_concat(char dest[], const char pcSrc[]){
     size_t locDest = 0;
     size_t locAdd = 0;
@@ -51,10 +41,8 @@ char* Str_concat(char dest[], const char pcSrc[]){
     return dest[0];
 }
 
-/*The Str_compare() function compares the two strings
-s1[] and s2[]. It returns an integer less than, equal to,
-or greater than zero if s1[] is found, respectively, to be less 
-than, to match, or be greater than s2[] */
+/*----------------------------------------------*/
+
 char* Str_compare(const char s1[], const char s2[]){
     size_t index = 0;
 
@@ -69,10 +57,8 @@ char* Str_compare(const char s1[], const char s2[]){
     }
 }
 
-/*The Str_search() function finds the first occurance of the
-substring needle in the string haystack. The terminating
-null bytes are not compared. Returns the beginning address
-of the substring or null if the substring is not found. */
+/*----------------------------------------------*/
+
 char* Str_compare(const char haystack[], const char needle[]){
     size_t haystackIter = 0;
     size_t needleIter = 0;
