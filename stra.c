3a247a5f03155785714c2da132710a7e04c3e6c9
a4amber <aachow@princeton.edu>
2022-10-04 00:12:53 -0400

    misnamed str_search

--- stra.c
+++ stra.c
@@ -59,7 +59,7 @@ int Str_compare(const char s1[], const char s2[]){
 
 /*----------------------------------------------*/
 
-char* Str_compare(const char haystack[], const char needle[]){
+char* Str_search(const char haystack[], const char needle[]){
     size_t haystackIter = 0;
     size_t needleIter = 0;
 
