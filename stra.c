95267689373b2a20b30fb4516a6a1d2cc0fbc98d
a4amber <aachow@princeton.edu>
2022-10-07 21:35:21 -0400

    search boundary check

--- stra.c
+++ stra.c
@@ -98,11 +98,10 @@ char* Str_search(const char haystack[], const char needle[]){
 
     /*helps one of the boundary tests where needle is an empty string*/
     if(needle[0] == '\0'){
-        /*
     while (haystack[haystackIter] != '\0'){
     haystackIter++;
-    } */
-    return (char*) haystack + haystackIter;
+    } 
+    return (char*) haystack /*+ haystackIter*/;
     }
     
 
